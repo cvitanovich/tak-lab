a303c436247c62d62696629c96047051262f229a
Andrew Cvitanovich <cvitanovich@gmail.com>
2013-10-14 07:59:26 -0700

    Making daily analysis script (spatial PDR)

--- main/utils/whnoise.m
+++ main/utils/whnoise.m
@@ -3,6 +3,7 @@ function [X,Xtime] = whnoise(minfreq,maxfreq,Fs,duration,phi)
 %[X,Xtime] = whnoise(minfreq,maxfreq,Fs,duration,phi)
 %adapted from Kip Keller w_rnd.m
 
+
 if (nargin < 3) Fs = 30000; end
 if (nargin < 4) duration = .1; end
 if (nargin < 5) phase = (rand(duration*Fs,1) - 0.5) * (2*pi);
@@ -31,42 +32,42 @@ Xtime=Xtime/max(abs(Xtime));						%have to normalize so that max and mins are be
 
 
 while 0
-valnoise = Xtime;
-valnoise = abs(hilbert(valnoise));
-valnoise = valnoise*10000;
-valnoise = valnoise-mean(valnoise);
-figure
-% Use next highest power of 2 greater than or equal to length(x) to calculate FFT.
-nfft= 2^(nextpow2(length(valnoise))); 
-% Take fft, padding with zeros so that length(fftx) is equal to nfft 
-fftx = fft(valnoise,nfft); 
-% Calculate the numberof unique points
-NumUniquePts = ceil((nfft+1)/2); 
-% FFT is symmetric, throw away second half 
-fftx = fftx(1:NumUniquePts); 
-% Take the magnitude of fft of x and scale the fft so that it is not a function of the length of x
-mx = abs(fftx)/length(valnoise); 
-% Take the square of the magnitude of fft of x. 
-mx = mx.^2; 
-% Since we dropped half the FFT, we multiply mx by 2 to keep the same energy.
-% The DC component and Nyquist component, if it exists, are unique and should not be multiplied by 2.
-if rem(nfft, 2) % odd nfft excludes Nyquist point
-  mx(2:end) = mx(2:end)*2;
-else
-  mx(2:end -1) = mx(2:end -1)*2;
-end
-% This is an evenly spaced frequency vector with NumUniquePts points. 
-f = (0:NumUniquePts-1)*Fs/nfft; 
-% normalize mx
-mx = mx/max(mx);
-
-mx = 20*log10(mx);
-% Generate the plot, title and labels. 
-plot(f,mx);
-axis([0 20000 -100 100])
-title('Power Spectrum'); 
-xlabel('Frequency (Hz)');
-ylabel('Power');
+    valnoise = Xtime;
+    valnoise = abs(hilbert(valnoise));
+    valnoise = valnoise*10000;
+    valnoise = valnoise-mean(valnoise);
+    figure
+    % Use next highest power of 2 greater than or equal to length(x) to calculate FFT.
+    nfft= 2^(nextpow2(length(valnoise)));
+    % Take fft, padding with zeros so that length(fftx) is equal to nfft
+    fftx = fft(valnoise,nfft);
+    % Calculate the numberof unique points
+    NumUniquePts = ceil((nfft+1)/2);
+    % FFT is symmetric, throw away second half
+    fftx = fftx(1:NumUniquePts);
+    % Take the magnitude of fft of x and scale the fft so that it is not a function of the length of x
+    mx = abs(fftx)/length(valnoise);
+    % Take the square of the magnitude of fft of x.
+    mx = mx.^2;
+    % Since we dropped half the FFT, we multiply mx by 2 to keep the same energy.
+    % The DC component and Nyquist component, if it exists, are unique and should not be multiplied by 2.
+    if rem(nfft, 2) % odd nfft excludes Nyquist point
+        mx(2:end) = mx(2:end)*2;
+    else
+        mx(2:end -1) = mx(2:end -1)*2;
+    end
+    % This is an evenly spaced frequency vector with NumUniquePts points.
+    f = (0:NumUniquePts-1)*Fs/nfft;
+    % normalize mx
+    mx = mx/max(mx);
+    
+    mx = 20*log10(mx);
+    % Generate the plot, title and labels.
+    plot(f,mx);
+    axis([0 20000 -100 100])
+    title('Power Spectrum');
+    xlabel('Frequency (Hz)');
+    ylabel('Power');
 end
 
 return;
